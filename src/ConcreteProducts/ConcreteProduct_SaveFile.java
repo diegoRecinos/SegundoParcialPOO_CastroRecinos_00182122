32c33669958d753cf3f41580ba1f843b71a31409
DierRec <00182122@uca.edu.sv>
2024-06-14 16:00:36 -0600

    Sistema de notificacion AbstractFactory

--- src/ConcreteProducts/ConcreteProduct_SaveFile.java
+++ src/ConcreteProducts/ConcreteProduct_SaveFile.java
@@ -1,9 +1,8 @@
 package src.ConcreteProducts;
 
-
 import src.Interface_ConcreteProducts.Interface_ProductTypeC_saveFile;
 
-public class ConcreteProduct_WindowsCheckbox implements Interface_ProductTypeC_saveFile {
+public class ConcreteProduct_SaveFile implements Interface_ProductTypeC_saveFile {
     
     @Override
     public void save() {
